9c5f476dd50967587b0bfe22f076034e330c933a
ima2003626 <162275772+ima2003626@users.noreply.github.com>
2025-04-15 23:38:36 +0530

    Delete Backend directory

--- Backend/demo/src/main/java/com/springbootakithimsara/demo/repo/MaterialRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springbootakithimsara.demo.repo;
-
-import com.springbootakithimsara.demo.model.Material;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MaterialRepo extends JpaRepository<Material, Long> {
-}
